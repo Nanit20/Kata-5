28d4498f4154d88ccb559fe685104ccf4fc4726d
Usuario <Usuario@10.22.148.82>
2019-11-13 09:37:18 +0000

    Kata 5

--- Kata 5/src/kata/pkg5/Kata5.java
+++ Kata 5/src/kata/pkg5/Kata5.java
@@ -7,28 +7,30 @@ package kata.pkg5;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
  * @author Usuario
  */
 public class Kata5 {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         String URL_BD_SQLite = new String ("jdbc:sqlite:C:\\Users\\Usuario\\Documents\\NetBeansProjects\\DB_SQLite");
-        connect (URL_BD_SQLite);
-        selectData_PEOPLE();
+        Connection connection = connect (URL_BD_SQLite);
+        selectData_PEOPLE(connection);
     }
 
-    private static void connect(String URL_BD_SQLite) {
+    private static Connection connect(String URL_BD_SQLite) {
         Connection connection = null;
         try{
-            connection = DriverManager.getConnection(URL_BD_SQLite);
+            this.connection = DriverManager.getConnection(URL_BD_SQLite);
             System.out.println("Base de datos conectada...");
+            return connection;
         }
         catch(SQLException exception) {
             System.out.println("ERROR(SQLException)" + exception.getMessage());
@@ -43,6 +45,12 @@ public class Kata5 {
                 System.out.println("ERROR(SQLException)" + exception.getMessage());
             }
         }
+    }
+
+    private static void selectData_PEOPLE(Connection connection) {
+        String SQL = "SELECT * FROM PEOPLE";
+        Statement statement = connection.createStatement();
+        ResultSet resultset = statement.executeQuery(SQL);
     }
     
 }
