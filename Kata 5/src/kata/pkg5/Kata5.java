d22a5d0fe2f74f674da34fb99ce4bba74204a751
Usuario <Usuario@10.22.148.82>
2019-11-13 09:30:27 +0000

    Kata4 with Iterator pattern

--- Kata 5/src/kata/pkg5/Kata5.java
+++ Kata 5/src/kata/pkg5/Kata5.java
@@ -5,6 +5,10 @@
  */
 package kata.pkg5;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Usuario
@@ -15,7 +19,30 @@ public class Kata5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String URL_BD_SQLite = new String ("jdbc:sqlite:C:\\Users\\Usuario\\Documents\\NetBeansProjects\\DB_SQLite");
+        connect (URL_BD_SQLite);
+        selectData_PEOPLE();
+    }
+
+    private static void connect(String URL_BD_SQLite) {
+        Connection connection = null;
+        try{
+            connection = DriverManager.getConnection(URL_BD_SQLite);
+            System.out.println("Base de datos conectada...");
+        }
+        catch(SQLException exception) {
+            System.out.println("ERROR(SQLException)" + exception.getMessage());
+        }
+        finally{
+            try{
+                if(connection != null){
+                    connection.close();
+                }
+            }
+            catch(SQLException exception) {
+                System.out.println("ERROR(SQLException)" + exception.getMessage());
+            }
+        }
     }
     
 }
